a93fa78ae420b2d4f71df608ccae44bb07da2829
kadirbekova43 <kadirbekova43@gmail.com>
2023-06-15 10:00:32 +0600

    0000

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ poetry install
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-python manage.py createsuperuser --username <username> --password <password>
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('<username>', '<email>', '<password>')" | python manage.py shell
 
