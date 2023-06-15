d5ecbd43f828b17e4321de33c372959ee4c444f2
kadirbekova43 <kadirbekova43@gmail.com>
2023-06-15 07:30:00 +0600

    superuser

--- build.sh
+++ build.sh
@@ -4,3 +4,15 @@ poetry install
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Check if CREATE_SUPERUSER variable is set
+if [ ! -z "$CREATE_SUPERUSER" ]; then
+  # Create superuser
+  python manage.py createsuperuser --noinput \
+    --username=admin \
+    --email=admin@example.com \
+    --password=1
+fi
+
+# Start the server
+python manage.py runserver 0.0.0.0:$PORT
