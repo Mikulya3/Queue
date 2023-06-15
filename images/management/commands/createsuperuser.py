54b25c64f33ace51b7cfbc05ae697362a4a30b55
kadirbekova43 <kadirbekova43@gmail.com>
2023-06-15 12:24:04 +0600

    25

--- images/management/commands/createsuperuser.py
+++ images/management/commands/createsuperuser.py
@@ -1,7 +1,5 @@
-
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
-
 
 class Command(BaseCommand):
     help = 'Creates a superuser.'
@@ -10,6 +8,7 @@ class Command(BaseCommand):
         if not User.objects.filter(username='admin').exists():
             User.objects.create_superuser(
                 username='admin',
-                password='complexpassword123'
+                password='complexpassword123',
+                email='admin@example.com'
             )
         print('Superuser has been created.')
