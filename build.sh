7b2acd35093c2a15870ca6ed1fe72f003e4a82b5
kadirbekova43 <kadirbekova43@gmail.com>
2023-06-15 08:30:47 +0600

    supe

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 python manage.py createsuperuser --no-input
+
+python manage.py createsuperuser2 --username admin2 --password 123456
