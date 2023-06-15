f1ca40fec2c817bbcaf1ee61045d0779c672f477
kadirbekova43 <kadirbekova43@gmail.com>
2023-06-15 09:44:03 +0600

    00

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ poetry install
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-python manage.py createsuperuser --username admin2 --password mysecretadminpassword
+python manage.py createsuperuser --username admin2 --password 1
