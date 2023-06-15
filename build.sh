9bacb1fc28a288048847b5f70187090d3fac857b
kadirbekova43 <kadirbekova43@gmail.com>
2023-06-15 07:44:39 +0600

    su

--- build.sh
+++ build.sh
@@ -5,11 +5,7 @@ poetry install
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# Check if CREATE_SUPERUSER variable is set
-if [ ! -z "$CREATE_SUPERUSER" ]; then
-  # Create superuser
-  echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'password')" | python manage.py shell
+if [[ $CREATE_SUPERUSER ]];
+then
+  python world_champ_2022/manage.py createsuperuser --no-input
 fi
-
-# Start the server
-python manage.py runserver 0.0.0.0:$PORT
