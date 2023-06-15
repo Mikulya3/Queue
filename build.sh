7e04d73d643c0dcfb67baa39b5b5aee092ed7193
mikulya43 <mikulya43@gmail.com>
2023-06-15 20:03:13 +0600

    аээ

--- build.sh
+++ build.sh
@@ -4,13 +4,4 @@ poetry install
 python manage.py collectstatic --no-input
 python manage.py migrate
 # Run createsuperuser command
-python manage.py shell <<EOF
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser(
-        username='admin',
-        password='complexpassword123',
-        email='admin@example.com'
-    )
-EOF
+python manage.py createsuperuser
